4d5948968e19ef9e77a1bee50ccdb3171c67fc10
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2019-02-23 23:48:41 +0700

    add card style

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -14,5 +14,22 @@ export const styles = StyleSheet.create({
   label: {
     color: 'black',
     fontSize: 16
+  },
+  card: {
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#ddd',
+    borderBottomWidth: 1,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 0.5,
+    padding: 16,
+    marginVertical: 4,
+    marginHorizontal: 8
   }
 });
